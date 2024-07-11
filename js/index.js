a2b79fb2bfc1b4b2805d25451013d9758cf5802e
zacWCCI <zac@wecanccodeit.org>
2024-07-11 12:37:48 -0400

    fix home

--- js/index.js
+++ js/index.js
@@ -3,4 +3,4 @@ const hash = window.location.hash.substring(1)
 if (hash) window.history.pushState({}, "", `${hash}`)
 
 // swap the htmx element and replace the url with hash if hash is present
-htmx.ajax("GET", hash ? hash : "/home.html", "#main")
+htmx.ajax("GET", hash ? hash : "/277/home.html", "#main")
