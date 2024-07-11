255c5a315927ab67dd8e8858a349c96dc7e200ba
zacWCCI <zac@wecanccodeit.org>
2024-07-11 11:42:30 -0400

    ditch the routing and use html extension

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // get hash from url and remove it
 const hash = window.location.hash.substring(1)
-window.history.pushState({}, "", `/${hash}`)
+if (hash) window.history.pushState({}, "", `${hash}`)
 
 // swap the htmx element and replace the url with hash if hash is present
-htmx.ajax("GET", hash ? hash : "home", "#main")
+htmx.ajax("GET", hash ? hash : "/home.html", "#main")
